0e92cb0541e4eaf0322e1285667e19fc83b5980b
Marciel <wentzke.m@gmail.com>
2025-05-08 15:54:39 +0200

    added a function to save a player and a function to convert a player obj to a DAO

--- cedh-tool-backend/src/main/java/de/balloncon/cedh_tool_backend/player/PlayerService.java
+++ cedh-tool-backend/src/main/java/de/balloncon/cedh_tool_backend/player/PlayerService.java
@@ -1,5 +1,6 @@
 package de.balloncon.cedh_tool_backend.player;
 
+import de.balloncon.cedh_tool_backend.dto.PlayerDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -18,5 +19,18 @@ public class PlayerService {
 
     public Player findPlayerById(UUID id) {
         return playerRepository.findById(id).orElse(null);
+    }
+
+    public void savePlayer(PlayerDto player) {
+        Player playerEntity = convertToDAO(player);
+        playerRepository.save(playerEntity);
+    }
+
+    private Player convertToDAO(PlayerDto playerDTO) {
+        Player player = new Player();
+        player.setNickname(playerDTO.nickname());
+        player.setLastname(playerDTO.lastname());
+        player.setFirstname(playerDTO.firstname());
+        return player;
     }
 }
