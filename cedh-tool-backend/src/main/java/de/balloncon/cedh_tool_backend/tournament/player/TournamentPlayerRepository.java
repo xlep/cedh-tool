3bacbf775071241884c14b280769967c0d15e394
Marciel <wentzke.m@gmail.com>
2025-05-08 15:36:31 +0200

    Added TournamentPlayerRepository to the application

---
+++ cedh-tool-backend/src/main/java/de/balloncon/cedh_tool_backend/tournament/player/TournamentPlayerRepository.java
@@ -0,0 +1,28 @@
+package de.balloncon.cedh_tool_backend.tournament.player;
+
+import de.balloncon.cedh_tool_backend.player.Player;
+import de.balloncon.cedh_tool_backend.player.score.view.PlayerScoreView;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface TournamentPlayerRepository extends JpaRepository <TournamentPlayer, UUID> {
+    @Query(value = """
+        SELECT tp.score as score, 
+               p.nickname as nickname, 
+               p.firstname as firstname, 
+               p.lastname as lastname
+        FROM tournamentplayers tp
+        JOIN player p ON tp.player = p.id
+        WHERE tp.tournament = :tournamentId
+        """, nativeQuery = true)
+    List<PlayerScoreView> findPlayerScoresByTournament(@Param("tournamentId") UUID tournamentId);
+
+    @Query("SELECT tp.player FROM TournamentPlayer tp WHERE tp.tournament.id = :tournamentId")
+    List<Player> findPlayersByTournamentId(@Param("tournamentId") UUID tournamentId);
+}
