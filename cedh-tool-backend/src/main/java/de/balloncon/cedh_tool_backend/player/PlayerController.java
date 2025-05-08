d06721605c5a31eec044de871a18ae1076d6f34f
Marciel <wentzke.m@gmail.com>
2025-05-08 15:53:48 +0200

    added an annotation for local testing added an endpoint to create players

--- cedh-tool-backend/src/main/java/de/balloncon/cedh_tool_backend/player/PlayerController.java
+++ cedh-tool-backend/src/main/java/de/balloncon/cedh_tool_backend/player/PlayerController.java
@@ -1,13 +1,13 @@
 package de.balloncon.cedh_tool_backend.player;
 
-import de.balloncon.cedh_tool_backend.pod.PodService;
+import de.balloncon.cedh_tool_backend.dto.PlayerDto;
 import io.swagger.v3.oas.annotations.Parameter;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("${apiVersion}")
 public class PlayerController {
@@ -21,5 +21,11 @@ public class PlayerController {
     @GetMapping("player")
     Player player(@Parameter UUID playerId) {
         return playerService.findPlayerById(playerId);
+    }
+
+    @PostMapping("player")
+    public ResponseEntity<String> createPlayer(@RequestBody PlayerDto playerDto) {
+        playerService.savePlayer(playerDto);
+        return ResponseEntity.ok("Player created successfully");
     }
 }
