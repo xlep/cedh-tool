bd46d03d6e1f6150780b351d2a1e05f3bcf8bd6a
Marciel <wentzke.m@gmail.com>
2025-05-09 09:16:59 +0200

    added TournamentPlayerScoreView to get the score of a player in a tournament.

---
+++ cedh-tool-backend/src/main/java/de/balloncon/cedh_tool_backend/tournament/player/score/view/TournamentPlayerScoreView.java
@@ -0,0 +1,9 @@
+package de.balloncon.cedh_tool_backend.tournament.player.score.view;
+
+public interface TournamentPlayerScoreView {
+    int getScore();
+    String getNickname();
+    String getFirstname();
+    String getLastname();
+}
+
