d679815e25fea5bd34def1567526484280451b55
Marciel <wentzke.m@gmail.com>
2025-05-09 13:39:36 +0200

    Added a mapping from pod to tournament in Pod

--- cedh-tool-backend/src/main/java/de/balloncon/cedh_tool_backend/pod/Pod.java
+++ cedh-tool-backend/src/main/java/de/balloncon/cedh_tool_backend/pod/Pod.java
@@ -2,6 +2,8 @@ package de.balloncon.cedh_tool_backend.pod;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import de.balloncon.cedh_tool_backend.seat.Seat;
+import de.balloncon.cedh_tool_backend.tournament.Tournament;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,6 +22,10 @@ public class Pod {
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id", nullable = false)
     private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "tournament_id", nullable = false)
+    private Tournament tournament;
 
     @Column(name = "name")
     private Integer name;
