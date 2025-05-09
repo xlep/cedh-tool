9567d48a719fa80462ee0f7a43c462f2b3d735a0
Marciel <wentzke.m@gmail.com>
2025-05-09 09:18:56 +0200

    Added the TournamentController API.

---
+++ cedh-tool-backend/src/main/java/de/balloncon/cedh_tool_backend/tournament/TournamentController.java
@@ -0,0 +1,25 @@
+package de.balloncon.cedh_tool_backend.tournament;
+
+import de.balloncon.cedh_tool_backend.dto.RoundDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/v1/tournaments")
+public class TournamentController {
+
+  private final TournamentService tournamentService;
+
+  TournamentController(TournamentService tournamentService) {
+    this.tournamentService = tournamentService;
+  }
+
+  @PostMapping("{tournamentId}/rounds")
+  public ResponseEntity<RoundDto> createNewRound(@PathVariable UUID tournamentId) {
+    RoundDto newRound = tournamentService.generateNextRound(tournamentId);
+    return ResponseEntity.ok(newRound);
+  }
+ }
