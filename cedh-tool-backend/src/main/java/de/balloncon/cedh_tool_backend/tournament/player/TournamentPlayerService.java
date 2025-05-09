1c88ca8157f9ffe1657b94291dffae27d26d7a96
Marciel <wentzke.m@gmail.com>
2025-05-09 09:18:42 +0200

    Added the TournamentPlayerService.

---
+++ cedh-tool-backend/src/main/java/de/balloncon/cedh_tool_backend/tournament/player/TournamentPlayerService.java
@@ -0,0 +1,23 @@
+package de.balloncon.cedh_tool_backend.tournament.player;
+
+import de.balloncon.cedh_tool_backend.tournament.player.score.view.TournamentPlayerScoreView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class TournamentPlayerService {
+
+    @Autowired
+    private final TournamentPlayerRepository repository;
+
+    public TournamentPlayerService(TournamentPlayerRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<TournamentPlayerScoreView> getPlayerScoresByTournament(UUID tournamentId) {
+        return repository.findPlayerScoresByTournament(tournamentId);
+    }
+}
