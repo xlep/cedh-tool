0312237028d7a62bd0aa3ae9e0f7f25a707acf43
Marciel <wentzke.m@gmail.com>
2025-05-12 22:11:36 +0200

    fixed a wrong string in RequestMapping

--- cedh-tool-backend/src/main/java/de/balloncon/cedh_tool_backend/tournament/TournamentController.java
+++ cedh-tool-backend/src/main/java/de/balloncon/cedh_tool_backend/tournament/TournamentController.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/${apiVersion}/tournaments")
+@RequestMapping("${apiVersion}/tournaments")
 public class TournamentController {
 
   private final TournamentService tournamentService;
