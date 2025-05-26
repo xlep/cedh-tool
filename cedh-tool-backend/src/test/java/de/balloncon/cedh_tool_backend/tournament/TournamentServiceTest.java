a6d29798c12bcfb5f1269c88eb91184b3117d968
Marciel <wentzke.m@gmail.com>
2025-05-26 21:26:01 +0200

    adaption in unittests

--- cedh-tool-backend/src/test/java/de/balloncon/cedh_tool_backend/tournament/TournamentServiceTest.java
+++ cedh-tool-backend/src/test/java/de/balloncon/cedh_tool_backend/tournament/TournamentServiceTest.java
@@ -94,7 +94,7 @@ class TournamentServiceTest {
     TestTransaction.start();
 
     List<Pod> pods = podService.getPodsAndSeatsByTournamentId(tournamentId);
-    List<PlayerDto> tournamentPlayers = tournamentPlayerService.getPlayersById(tournamentId);
+    List<PlayerDto> tournamentPlayers = tournamentPlayerService.getPlayerDtosByTournament(tournamentId);
 
     // We have 26 players, so we should have 7 pods (with 1 BYE pod containing 2 players
     assertThat(pods).isNotEmpty().hasSize(7);
