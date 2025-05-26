ce93d3eec5a7461861d69f974a0da02b291ccac2
Marciel <wentzke.m@gmail.com>
2025-05-26 21:25:10 +0200

    added queues to the repositorys

--- cedh-tool-backend/src/main/java/de/balloncon/cedh_tool_backend/player/PlayerRepository.java
+++ cedh-tool-backend/src/main/java/de/balloncon/cedh_tool_backend/player/PlayerRepository.java
@@ -1,10 +1,17 @@
 package de.balloncon.cedh_tool_backend.player;
 
+import java.util.List;
 import java.util.UUID;
+import de.balloncon.cedh_tool_backend.tournament.player.TournamentPlayer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, UUID> {
+
+  @Query("SELECT tp.player FROM TournamentPlayer tp WHERE tp.tournament.id = :tournamentId")
+  List<Player> findByTournamentId(@Param("tournamentId") UUID tournamentId);
 
 }
